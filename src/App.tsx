d09824a19cf990bf48b685088c5348681792d8c3
inaciolimaf <92096585+inaciolimaf@users.noreply.github.com>
2023-12-17 14:55:41 -0300

    ADD: nav e inicio do header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from './layouts/home.tsx'
+import HeaderNav from './layouts/header.tsx'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <HeaderNav/>
+      <Home />
+    </>
+  )
+}
+
